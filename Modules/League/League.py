bc0993d157dcb8d7293f68c6a16c8dbf579c8e69
Ryan <45923745+RyanDBurke@users.noreply.github.com>
2026-01-06 02:57:35 -0500

    added all teams in league to the League object

--- Modules/League/League.py
+++ Modules/League/League.py
@@ -1,8 +1,10 @@
 from Modules.Enums.Sport import Sport
+from Modules.League.Team import Team
 
 class League:
-    def __init__(self, league_id: int, sport: Sport, season_year: str, name: str):
+    def __init__(self, league_id: str, sport: Sport, season_year: str, name: str, teams: list[Team]):
         self.league_id = league_id
         self.sport = sport
         self.season_year = season_year
         self.name = name
+        self.teams = teams
